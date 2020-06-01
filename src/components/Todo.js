20964d8f976f4be9f58928f51201f66d5060dbed
Marta Krawczyk <martaalicja113@gmail.com>
2020-06-01 19:34:31 -0400

    minor styling changes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,9 @@ const Todo = props => {
         <div onClick={() => props.toggleTask(props.item.id)} 
         // className={`task ${props.item.completed ? "completed" : ""}`} 
         style={{textDecoration:`${props.item.completed ? 'line-through' : 'none'}`, 
-        backgroundColor: `${props.item.completed ? '#d17d7c' : ''}`}}>
+        backgroundColor: `${props.item.completed ? '#d17d7c' : 'rgb(49, 41, 59, .8)'}`,
+        cursor: 'pointer',
+        color: `${props.item.completed ? '' : 'white'}`}}>
 <p>{props.item.task}</p>
         </div>
     )
